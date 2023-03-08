1ebd05057abb29c5aa377871e528b5240c65efbb
Popov Philipp <philolippo@gmail.com>
2023-03-09 00:10:28 +0100

    Implement image rotation based on EXIF metadata

--- src/image_transforms.rs
+++ src/image_transforms.rs
@@ -1,7 +1,28 @@
-use image::imageops::{Nearest, Triangle};
+use image::imageops::Triangle;
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, GenericImageView, ImageFormat};
-use std::{fs, os::unix::fs::MetadataExt};
+use std::{fs, io::BufReader, os::unix::fs::MetadataExt};
+
+use exif;
+
+pub fn read_exif_metadata(image_path: &str) -> Option<u32> {
+    let file = fs::File::open(image_path).unwrap();
+
+    let exif_metadata = exif::Reader::new().read_from_container(&mut BufReader::new(&file));
+
+    // If read_from_container results in Err, this means that there is no EXIF metadata
+    // https://github.com/kamadak/exif-rs/issues/24
+    match exif_metadata {
+        Ok(exif_metadata) => {
+            let orientation = exif_metadata.get_field(exif::Tag::Orientation, exif::In::PRIMARY);
+            match orientation {
+                Some(orientation) => orientation.value.get_uint(0),
+                None => None,
+            }
+        }
+        Err(_) => None,
+    }
+}
 
 pub fn check_encoded_size(image_path: &str) -> std::io::Result<u64> {
     let meta = fs::metadata(image_path)?;
@@ -10,14 +31,27 @@ pub fn check_encoded_size(image_path: &str) -> std::io::Result<u64> {
 }
 
 pub fn process_image(image_path: &str, image_size: u64) -> image::ImageResult<()> {
+    let orientation = read_exif_metadata(image_path);
     let image = read_image(image_path)?;
     let ratio = compute_ratio_fast(image_size);
 
     let dimensions = image.dimensions();
     let new_width = (dimensions.0 as f64 / ratio) as u32;
     let new_height = (dimensions.1 as f64 / ratio) as u32;
-    let tmp_image = image.resize(new_width, new_height, Nearest);
-    tmp_image.save_with_format(image_path, ImageFormat::Jpeg)?;
+    let tmp_image = image.resize(new_width, new_height, Triangle);
+
+    let rotated_image: DynamicImage = match orientation {
+        Some(2) => tmp_image.fliph(),
+        Some(3) => tmp_image.rotate180(),
+        Some(4) => tmp_image.flipv(),
+        Some(5) => tmp_image.rotate90().fliph(),
+        Some(6) => tmp_image.rotate90(),
+        Some(7) => tmp_image.rotate270().fliph(),
+        Some(8) => tmp_image.rotate270(),
+        _ => tmp_image,
+    };
+
+    rotated_image.save_with_format(image_path, ImageFormat::Jpeg)?;
     Ok(())
 }
 
@@ -28,27 +62,4 @@ pub fn read_image(image_path: &str) -> image::ImageResult<DynamicImage> {
 
 pub fn compute_ratio_fast(image_size: u64) -> f64 {
     return image_size as f64 / 1000000.0;
-}
-
-fn compute_ratio_slow(image: DynamicImage) -> f32 {
-    // This is omega slow. Maybe do a binary search ?
-
-    let mut ratio = 2.5;
-    let max_image_size = 0;
-    let dimensions = image.dimensions();
-
-    while ratio > 1.3 && max_image_size < 1000000 {
-        ratio = ratio - 0.2;
-        let new_width = (dimensions.0 as f32 / ratio) as u32;
-        let new_height = (dimensions.1 as f32 / ratio) as u32;
-        let _tmp_image = image.resize(new_width, new_height, Triangle);
-        //let mut buff = Vec::new();
-        //tmp_image
-        //.write_to(&mut buff, image::ImageFormat::Jpeg)
-        //.unwrap();
-        //max_image_size = buff.len();
-        //println!("Ratio {}, image_size {} bytes", ratio, max_image_size);
-    }
-
-    return ratio;
 }
